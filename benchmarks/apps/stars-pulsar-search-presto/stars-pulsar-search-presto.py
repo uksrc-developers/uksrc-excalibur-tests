ae50e686ec0d73254e79407cbeb65aba4b3836f7
Owain Kenway <o.kenway@ucl.ac.uk>
2026-06-01 16:36:03 +0100

    Need to set DATA=big

--- benchmarks/apps/stars-pulsar-search-presto/stars-pulsar-search-presto.py
+++ benchmarks/apps/stars-pulsar-search-presto/stars-pulsar-search-presto.py
@@ -70,6 +70,8 @@ class STARSpulsarsearchpresto(ContainerTest):
         os.mkdir(os.path.join(self.outputdir, "logs"))
         self.executable_opts = [
             "exec",
+            "--env",
+            "DATA=big"
             "--no-home",
             "--bind",
             f"{self.outputdir}:/output",
