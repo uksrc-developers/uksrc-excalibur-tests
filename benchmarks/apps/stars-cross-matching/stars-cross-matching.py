fbba8c53b90fa049feb4aebf82de75b338d903a9
Owain Kenway <o.kenway@ucl.ac.uk>
2026-06-03 16:02:07 +0100

    Maybe we can't have a - in there

--- benchmarks/apps/stars-cross-matching/stars-cross-matching.py
+++ benchmarks/apps/stars-cross-matching/stars-cross-matching.py
@@ -10,7 +10,7 @@ from astropy.io import fits
 
 @rfm.simple_test
 class STARScrossmatch(STARSTest):
-    stars_name="cross-matching"
+    stars_name="crossmatching"
     container_url = "docker://registry.gitlab.com/ska-telescope/src/src-workloads/cross-matching"
 
     @sanity_function
